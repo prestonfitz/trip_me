2f22b7e3d97fc3d5a993a40a942e14262bb8edfb
Preston Fitzgerald <prestonfitz@gmail.com>
2023-04-15 11:36:43 -0600

    redeploy

--- travelPages/models.py
+++ travelPages/models.py
@@ -2,12 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-'''class Trip(models.Model):
-
-    city = models.CharField(max_length=20)
-    state = models.CharField(max_length=20, blank=True)
-    country = models.CharField(max_length=20)
-    
-
-    def __str__(self):
-        return (self.city)'''
