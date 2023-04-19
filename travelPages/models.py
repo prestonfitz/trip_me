d3692eba7ca5eff03f33a4447c32fe25aaea7a17
Preston Fitzgerald <prestonfitz@gmail.com>
2023-04-19 13:48:19 -0600

    Database

--- travelPages/models.py
+++ travelPages/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class tripInfo(models.Model):
+
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30,null=True,blank=True)
+    country = models.CharField(max_length=30)
+    description = models.TextField(max_length=1000)
+    length = models.IntegerField(default=0)
+    cost = models.FloatField(default=0.00)
+    picture = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+    
+    def location(self):
+        return '%s %s' (self.city, self.country)
--- trip_me/settings.py
+++ trip_me/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,10 @@ WSGI_APPLICATION = 'trip_me.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
