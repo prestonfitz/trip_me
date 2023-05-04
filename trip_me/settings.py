e816e10b0247a5a4d43b4bf4e956f0806413c9fd
Preston Fitzgerald <prestonfitz@gmail.com>
2023-05-04 09:42:48 -0600

    Again

--- trip_me/settings.py
+++ trip_me/settings.py
@@ -11,6 +11,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
+import environ
+
+env=environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,13 +80,16 @@ WSGI_APPLICATION = 'trip_me.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-ENGINE = os.getenv("DATA_ENGINE")
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DATABASES = {
-    "default": {
-        'ENGINE':ENGINE,
-        'URL':DATABASE_URL,
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
