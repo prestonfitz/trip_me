1186ff375125811a5e4d49160dbbf6ff09c3e8dd
Preston Fitzgerald <prestonfitz@gmail.com>
2023-05-04 09:17:35 -0600

    Database experiments

--- trip_me/settings.py
+++ trip_me/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'trip_me.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+ENGINE = os.getenv("DATA_ENGINE")
 
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    "default": {
+        'ENGINE':ENGINE,
+        'URL':DATABASE_URL,
+    }
 }
 
 
