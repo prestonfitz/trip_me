d0b09661ce3bf6d63869be9dde94958c7eda9448
Preston Fitzgerald <prestonfitz@gmail.com>
2023-05-05 10:09:43 -0600

    admin updates

--- trip_me/settings.py
+++ trip_me/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-#CSRF_TRUSTED_ORIGINS = ['http://web-production-066e.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://tripme-production.up.railway.app/']
 
 
 # Application definition
