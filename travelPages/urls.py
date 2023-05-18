c07fafd81607d71c79f15883dfe6e823aca1872e
Preston Fitzgerald <prestonfitz@gmail.com>
2023-05-18 13:26:18 -0600

    Link updates

--- travelPages/urls.py
+++ travelPages/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import indexPageView, tripsPageView, detailsPageView
+from .views import indexPageView, tripsPageView, detailsPageView, twitter, facebook, linkedin
 
 app_name = 'travelPages'
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("trips/",tripsPageView,name="trips"),
     path("<int:id>/", detailsPageView, name='details'),
+    path("twitter/",twitter,name="twitter"),
+    path("facebook/",facebook,name="facebook"),
+    path("linkedin/",linkedin,name="linkedin")
 ]
--- travelPages/views.py
+++ travelPages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import tripInfo
 
 # Create your views here.
@@ -27,3 +27,12 @@ def detailsPageView(request, id):
    }
 
    return render(request,'travelPages/details.html', context)
+
+def twitter(request):
+    return redirect("http://twitter.com")
+
+def facebook(request):
+    return redirect("http://facebook.com")
+
+def linkedin(request):
+    return redirect("http://linkedin.com")
